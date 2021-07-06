3b9bbdac088114e1730385767c2f371367259fa4
jvrmalv <jvrmalv@gmail.com>
2021-07-06 20:32:38 -0300

    git-init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-container class="py-0 fill-height" align-center fluid>
+        <v-row class="" justify="center">
+          <v-col cols="12" md="6">
+            <todo-list
+              v-on:delete-todo="deleteTodo"
+              v-on:create-todo="createTodo($event.name, $event.description)"
+              v-on:edit-todo="editTodo"
+              class=""
+              :todolist="todosTest"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    TodoList,
+  },
+
+  data: () => ({
+    todosTest: [],
+  }),
+  mounted() {
+    axios.get("http://localhost:3000/todo/").then((response) => {
+      console.log(response);
+      this.todosTest = response.data;
+    });
+  },
+  methods: {
+    editTodo({ id, name, description }) {
+      console.log(id, name, description);
+      const refreshUpdatedTodo = (todo) => (acc, v) => {
+        if (v.id === id) return [...acc, todo];
+        return [...acc, v];
+      };
+      axios
+        .put(`http://localhost:3000/todo/${id}`, { name, description })
+        .then((res) => {
+          const todo = res.data;
+          console.log("Update todo:", todo);
+          this.todosTest = this.todosTest.reduce(refreshUpdatedTodo(todo), []);
+          console.log("Todoslist:", this.todosTest);
+        });
+    },
+    deleteTodo: function (id) {
+      console.log("Listened id is:", id);
+      axios
+        .delete(`http://localhost:3000/todo/${id}`)
+        .then(
+          () =>
+            (this.todosTest = this.todosTest.filter((todo) => todo.id !== id))
+        );
+    },
+    createTodo: function (name, description) {
+      const todo = { name, description };
+      axios
+        .post("http://localhost:3000/todo/", todo)
+        .then((res) =>
+          this.todosTest.push({
+            id: res.data.id,
+            name: todo.name,
+            description: todo.description,
+          })
+        );
+    },
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container class="grey darken-4">
+    <v-row class="align-center">
+      <v-col cols="4" dark class="d-flex justify-center">Name</v-col>
+      <v-col cols="7" class="d-flex justify-center">Desciption</v-col>
+      <v-col cols="1" class="d-flex justify-center">
+        <v-btn @click.stop="createDialog = true"
+          ><v-icon>mdi-note-plus</v-icon></v-btn
+        >
+        <v-dialog v-model="createDialog" max-width="500">
+          <v-card>
+            <v-card-title>Create todo</v-card-title>
+            <v-card-actions>
+              <v-text-field
+                v-model="createTodoProps.name"
+                label="Todo name"
+              ></v-text-field>
+              <v-spacer></v-spacer>
+              <v-text-field
+                v-model="createTodoProps.description"
+                label="Todo description"
+              ></v-text-field>
+              <v-spacer></v-spacer>
+              <v-btn v-on:click="$emit('create-todo', createTodoProps)"
+                >SUBMIT</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <v-row class="grey darken-3" v-for="i in todolist" :key="i.id">
+      <v-col cols="4"
+        ><p dark class="d-flex justify-center">{{ i.name }}</p></v-col
+      >
+      <v-divider vertical></v-divider>
+      <v-col cols="6" class="d-flex justify-center">{{ i.description }}</v-col>
+      <v-col cols="1" class="d-flex justify-center">
+        <v-btn icon class="red darken-2" v-on:click.stop="editDialog = true"
+          ><v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-dialog v-model="editDialog" max-width="500">
+          <v-card>
+            <v-card-title>Edit todo</v-card-title>
+            <v-card-actions>
+              <v-text-field
+                v-model="editTodoProps.name"
+                label="Todo name"
+              ></v-text-field>
+              <v-spacer></v-spacer>
+              <v-text-field
+                v-model="editTodoProps.description"
+                label="Todo description"
+              ></v-text-field>
+              <v-spacer></v-spacer>
+              <v-btn
+                v-on:click="
+                  $emit('edit-todo', {
+                    id: i.id,
+                    name: editTodoProps.name,
+                    description: editTodoProps.description,
+                  })
+                "
+                >SUBMIT</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+      <v-col cols="1" class="d-flex justify-center">
+        <v-btn icon class="red darken-2" v-on:click="$emit('delete-todo', i.id)"
+          ><v-icon>mdi-trash-can</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      createTodoProps: {
+        name: null,
+        description: null,
+      },
+      editTodoProps: {
+        name: null,
+        description: null,
+      },
+      createDialog: false,
+      editDialog: false,
+    };
+  },
+  components: {},
+  props: ["todolist"],
+  mounted() {
+    console.log(this.todolist);
+  },
+};
+</script>
+
+<style>
+</style>
